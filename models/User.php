4bf780fc1293375e547cd8cbbdfe9e94a49afc06
Forklein <giuseppepisani2@gmail.com>
2021-10-06 15:29:17 +0200

    Add class User

---
+++ models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+class User
+{
+    protected $name;
+    protected $lastName;
+    protected $email;
+
+    public function __construct($name, $lastName, $email)
+    {
+        $this->name = $name;
+        $this->lastName = $lastName;
+        $this->email = $email;
+    }
+
+    public function getFullName()
+    {
+        return $this->name . ' ' . $this->lastName;
+    }
+}
