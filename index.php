f9e22b6ab96328dba75f8cd0fe175a1133caae16
Forklein <giuseppepisani2@gmail.com>
2021-10-06 16:32:22 +0200

    Add style

--- index.php
+++ index.php
@@ -12,19 +12,20 @@ require_once __DIR__ . '/models/Premium.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
     <title>OOP Ereditarietà</title>
 </head>
 
-<body>
+<body class="bg-secondary">
     <main>
-        <section>
+        <section class="text-center">
             <? $user = new User('Giuseppe', 'Pisani', 'test@libero.it') ?>
             <h1><?= $user->getFullName() ?></h1>
             <? $premium = new Premium('Giuseppe', 'Pisani', 'test@libero.it') ?>
             <h1><?= $premium->getDiscount() ?></h1>
             <?php
             try {
-                echo $premium->setCreditCard(50505050);
+                echo $premium->setCreditCard('ciao');
             } catch (Exception $e) {
                 echo $e->getMessage();
             }
